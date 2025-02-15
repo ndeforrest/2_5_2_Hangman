a89c36250f9c1d52b95bbdb1bbf440dfdd4829a3
Nolan <nolan.deforrest@student.tamdistrict.org>
2025-02-14 23:07:54 -0800

    guess methods Hangman

--- Game.java
+++ Game.java
@@ -5,59 +5,69 @@ public class Game {
 
     public Game() {
         phrase = WordProvider.getWord();
-        for(int i = 0; i < phrase.length(); i++) {
-            if (phrase.substring(i, i+1).equals(" ")) {
+        partial = "";
+        for (int i = 0; i < phrase.length(); i++) {
+            if (phrase.substring(i, i + 1).equals(" ")) {
                 partial += " ";
-            }
-            else {
-                partial+= "_";
+            } else {
+                partial += "_";
             }
         }
-        System.out.println(partial);    //print fully blank
-        points = (int)(Math.random() * 10);
+        System.out.println("Format: " + partial); // print fully blank
+        points = (int) (Math.random() * 10);
         System.out.println("Amount of points per guess: " + points);
     }
 
     public void guessLetter(String guess, Player player) {
         int count = 0;
-        for(int i = 0; i < phrase.length()-1; i++) {
-            if (phrase.substring(i, i+1).equals(guess)) {
+        for (int i = 0; i < phrase.length() - 1; i++) {
+            if (phrase.substring(i, i + 1).equals(guess)) {
                 count++;
                 partial = partial.substring(0, i) + guess + partial.substring(i);
-            }    
+            }
         }
         if (count == 0) {
+            System.out.println();
             player.switchTurn();
-
-        }
-        else {
+            System.out.println("The letter was not in the word.");
+        } else {
             player.addPoints(count * this.points);
-            System.out.println(partial);
+            System.out.println("The letter was in the word, you got " + count * this.points + "points.");
+            System.out.println("Heres the new phrase: " + partial);
         }
     }
 
     public boolean guessPhrase(String guess, Player player) {
         if (guess.equals(phrase)) {
-            System.out.println("Congrats! You got it!");
-            //reward 5x points
-            player.addPoints(5 * this.points);
+            System.out.println("Congrats! You got it! You are rewarded with " + points * 5 + "points.");
+            // reward 5x points
+            player.addPoints(5 * points);
             return true;
         }
         player.switchTurn();
         return false;
     }
-    
+
+    public void getGuess(String guess, Player player) {
+        if (guess.length() > 1) {
+            guessLetter(guess, player);
+        } else {
+            guessPhrase(guess, player);
+        }
+    }
 }
 
-
 /*
-    All players start with zero points.
-    A phrase is selected at random.
-    The structure of the phrase is displayed to the players.
-    A random point value is selected. 
-At the start of each turn, a player may guess a letter or solve the puzzle.
-If the player guesses a letter and it is in the phrase, the points are added to the player’s score, and the letters are revealed as a partially solved phrase. 
-If the letter is not in the phrase, the player loses their turn.
-If the player tries to solve the puzzle by typing in the entire phrase correctly, the game is over. If incorrect, the player loses their turn.
-Once the puzzle is solved, the player with the highest score wins.
+ * All players start with zero points.
+ * A phrase is selected at random.
+ * The structure of the phrase is displayed to the players.
+ * A random point value is selected.
+ * At the start of each turn, a player may guess a letter or solve the puzzle.
+ * If the player guesses a letter and it is in the phrase, the points are added
+ * to the player’s score, and the letters are revealed as a partially solved
+ * phrase.
+ * If the letter is not in the phrase, the player loses their turn.
+ * If the player tries to solve the puzzle by typing in the entire phrase
+ * correctly, the game is over. If incorrect, the player loses their turn.
+ * Once the puzzle is solved, the player with the highest score wins.
  */
--- Player.java
+++ Player.java
@@ -7,12 +7,20 @@ public class Player {
         this.name = name;
         points = 0;
     }
-    
+
     public void addPoints(int amt) {
         points += amt;
     }
 
     public void switchTurn() {
         turn = !turn;
+    }
+
+    public boolean getTurn() {
+        return turn;
+    }
+
+    public String getName() {
+        return name;
     }
 }
